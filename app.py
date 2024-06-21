aed387c93e20cefae440bab6c898e72f03ade582
Eje Joshua <eje_joshua@yahoo.com>
2024-06-21 15:30:32 +0100

    Styled job page and fix static path

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     jobs = load_jobs_from_db()
-    return render_template("home.html", jobs=jobs, company_name='Buzz')
+    return render_template("home.html", jobs=jobs)
 
 
 @app.route("/api/jobs")
@@ -19,7 +19,10 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id):
     job = load_job_from_db(id)
-    return jsonify(job)
+    if not job:
+        return "Not Found", 404
+    return render_template("jobpage.html", listed_job=job)
+
 
 @app.route("/learn")
 def learn():
