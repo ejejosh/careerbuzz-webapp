937c508d599c39de2ac99775fe59b7e88429192e
Eje Joshua <eje_joshua@yahoo.com>
2024-06-21 13:07:27 +0100

    modified database file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -15,6 +15,11 @@ def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
 
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    return jsonify(job)
 
 @app.route("/learn")
 def learn():
--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, select
+from sqlalchemy import create_engine, MetaData, Table, select, text
 from dotenv import load_dotenv, dotenv_values
 import os
 
@@ -30,3 +30,24 @@ def load_jobs_from_db():
             jobs.append(row_dict)
 
         return jobs
+
+
+def load_job_from_db(id):
+    # Define your raw SQL query
+    sql_query = text("SELECT * FROM jobs WHERE id = :job_id")
+
+    # Create a connection and execute the SQL query
+    with engine.connect() as connection:
+        result = connection.execute(sql_query, {'job_id': id})
+
+        # Fetch a single row
+        row = result.fetchone()
+
+        if row:
+            # Convert the row to a dictionary
+            row_dict = dict(row._mapping)
+
+            # Print the dictionary
+            return row_dict
+        else:
+            return None
