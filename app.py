041db8433e4a0219a06a6ee9911571ac81fe12aa
Eje Joshua <eje_joshua@yahoo.com>
2024-06-21 23:33:25 +0100

    Created application page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
@@ -16,14 +16,6 @@ def list_jobs():
     return jsonify(jobs)
 
 
-@app.route("/job/<id>")
-def show_job(id):
-    job = load_job_from_db(id)
-    if not job:
-        return "Not Found", 404
-    return render_template("jobpage.html", listed_job=job)
-
-
 @app.route("/learn")
 def learn():
     return render_template("learn.html")
@@ -37,6 +29,20 @@ def events():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return render_template("jobpage.html", listed_job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+    data = request.form
+    return jsonify(data)
 
 
 if __name__ == "__main__":
