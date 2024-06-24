3199c9a25610dfff519328763728dacaa464e9cf
Eje Joshua <eje_joshua@yahoo.com>
2024-06-24 11:13:53 +0100

    created funtion to add application to db

--- database.py
+++ database.py
@@ -51,3 +51,30 @@ def load_job_from_db(id):
             return row_dict
         else:
             return None
+
+
+def add_application_to_db(job_id, data):
+    # Defining raw SQL insert query
+    sql_insert = text("""
+                     INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_ur) 
+                     VALUES (:job_id. :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)
+    """)
+
+    # Data to insert
+    data_to_insert = {
+        'job_id': job_id,
+        'full_name': data['full_name'],
+        'email': data['email'],
+        'linkedin_url': data['linkedin_url'],
+        'education': data['education'],
+        'work_experience': data['work_experience'],
+        'resume_url': data['resume_url']
+    }
+
+    # Create a connection and execute the SQL insert query
+    with engine.connect() as connection:
+        result = connection.execute(sql_insert, data_to_insert)
+        
+        # Commit the transaction
+        connection.commit()
+
